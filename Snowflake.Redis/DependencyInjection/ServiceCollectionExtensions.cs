81d9629ad6e20a76152ebab254d12f5844ca1e22
HueiFeng <695979933@qq.com>
2021-08-13 17:32:42 +0800

    release Snowflake.Redis.CSharp 1.2.0

--- Snowflake.Redis/DependencyInjection/ServiceCollectionExtensions.cs
+++ Snowflake.Redis/DependencyInjection/ServiceCollectionExtensions.cs
@@ -7,13 +7,13 @@ namespace Snowflake.Redis.DependencyInjection
 {
     public static class ServiceCollectionExtensions
     {
-        public static IServiceCollection AddSnowflakeRedisService(this IServiceCollection services, string connectionString,Action<SnowflakeOptions> action)
+        public static IServiceCollection AddSnowflakeRedisService(this IServiceCollection services, string connectionString, Action<SnowflakeOptions> action)
         {
             RedisHelper.Initialization(new CSRedisClient(connectionString));
-            SnowflakeOptions snowflakeOptions=new SnowflakeOptions();
+            SnowflakeOptions snowflakeOptions = new SnowflakeOptions();
             action.Invoke(snowflakeOptions);
             var machineIdConfig = new MachineIdConfig(new RedisCacheAsync(), snowflakeOptions);
-            services.AddSingleton(typeof(SnowFlake), machineIdConfig.InitMachineId().Result);
+            services.AddTransient(e => machineIdConfig.InitMachineId().Result);
             return services;
         }
     }
